099a7a9ed6362a00a35b66f933f24a364f952830
zacanger <zac@zacanger.com>
2017-03-15 08:59:42 -0600

    Whoops.

--- src/ago.js
+++ src/ago.js
@@ -6,9 +6,10 @@ const ago = (d: Date): string => {
   return dayDiff === 0 && (
     diff < 60 && 'just now' ||
     diff < 120 && '1 minute ago' ||
-    diff < 3600 && Math.floor(diff / 60) + ' minutes ago' ||
+    diff < 3600 && `${Math.floor(diff / 60)} minutes ago` ||
     diff < 7200 && '1 hour ago' ||
-    diff < 86400 && Math.floor(diff / 3600) + ' hours ago') ||
+    diff < 86400 && `${Math.floor(diff / 3600)} hours ago`
+  ) ||
     dayDiff === 1 && 'Yesterday' ||
     dayDiff < 7 && `${dayDiff} days ago` ||
     dayDiff < 365 && `${Math.ceil(dayDiff / 7)} weeks ago` ||
