bce56faa18fa971a7b2f99c6b2c6246d0f18b21f
zacanger <zac@zacanger.com>
2017-05-10 19:17:24 -0600

    Update flow

--- dist/slice.js
+++ dist/slice.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var slice=function(a,b,c){return[].slice.call(a,b,c)};exports.default=slice;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var slice=function(a,b,c){return[].slice.call(a,b,c||1)};exports.default=slice;
--- src/where.js
+++ src/where.js
@@ -1,6 +1,8 @@
+// @flow
+
 import filter from './filter'
 
-const where = (arr, f) =>
+const where = (arr: any[], f: () => any): any =>
   filter(arr, f)[0] || null
 
 export default where
