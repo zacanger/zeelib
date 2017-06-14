f47710c88064865e99b30b04ef13cbf149a34bc8
zacanger <zac@zacanger.com>
2017-06-13 19:21:04 -0600

    Fix curry

--- src/curry.js
+++ src/curry.js
@@ -7,23 +7,42 @@
  * @returns {function}
  */
 
-const curry = (fn) => {
-  const originalArguments = fn
-    .toString()
-    .match(/\(.*?\)/)[0]
-    .replace(/[()]/gi, '')
-    .replace(/\s/gi, '')
-    .split(',')
-    .filter((x) => x)
+const curry = function (fn) {
+  const getFunctionArguments = function (fn) {
+    if (typeof fn !== 'function') {
+      throw new TypeError(`Expected argument to be a function! Received a ${typeof fn}.`)
+    }
 
-  const makeCurriedFunc = (...args) => {
-    const givenArguments = [ ...args ] || []
-    return givenArguments.length < originalArguments.length
-      ? (...rest) => makeCurriedFunc(...givenArguments, ...rest)
-      : fn(...givenArguments)
+    const functionAsString = fn.toString()
+
+    if (functionAsString) {
+      const m = functionAsString.match(/\(.*?\)/)
+      if (m && m[0]) {
+        const args = m[0]          // match everything between brackets
+          .replace(/[()]/gi, '')   // remove brackets
+          .replace(/\s/gi, '')     // remove all whitespace
+          .split(',')              // split on the commas
+
+        return args.filter((x) => x) // remove possible empty string from the result
+      }
+    }
   }
 
-  return (...args) => makeCurriedFunc(...args)
-}
+  const originalArguments = getFunctionArguments(fn) || []
 
+  const makeCurriedFunc = function () {
+    const givenArguments = arguments || []
+    if (givenArguments.length < originalArguments.length) {
+      return function (...rest) {
+        return makeCurriedFunc(...givenArguments, ...rest)
+      }
+    } else {
+      return fn(...givenArguments)
+    }
+  }
+
+  return function () {
+    return makeCurriedFunc(...arguments)
+  }
+}
 export default curry
