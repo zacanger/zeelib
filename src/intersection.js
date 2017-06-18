074eee977fbed4fc3354111e82caf916ca1416c6
zacanger <zac@zacanger.com>
2017-06-18 14:55:29 -0600

    a

--- src/intersection.js
+++ src/intersection.js
@@ -2,6 +2,9 @@
 
 /**
  * Get the intersection of two arrays
+ * @example
+ * intersection([ 1, 2 ], []) // => []
+ * intersection([ 1, 2, 3 ], [1, 2]) // => [ 1, 2 ]
  */
 
 const intersection = (xs: any[], ys: any[]): any[] =>
