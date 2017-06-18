67ef959f416c22b2ce4edc51db9237a2c82b30c0
zacanger <zac@zacanger.com>
2017-06-18 14:33:43 -0600

    a

--- src/cdar.js
+++ src/cdar.js
@@ -1,13 +1,13 @@
 // @flow
 
+import car from './car'
+import cdr from './cdr'
+
 /**
  * `cdar`
  * @example
  * cdar([ [ 1, 2 ], 3 ]) // => [ 2 ]
  */
-
-import car from './car'
-import cdr from './cdr'
 
 const cdar = (a: any[]): any =>
   cdr(car(a))
