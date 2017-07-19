03128e76b3a824b860b0eada26d6b00d513ea37e
zacanger <zac@zacanger.com>
2017-07-18 21:33:22 -0600

    Fix docs for 'keep'

--- dist/keep.js.flow
+++ dist/keep.js.flow
@@ -2,11 +2,12 @@ import id from './id'
 import filter from './filter'
 
 /**
- * Returns an array with all falsey values removed
- * @param {Array} a
- * @returns {Array}
+ * Returns an array or object with all falsey values removed
+ * @param {Array|Object} a
+ * @returns {Array|Object}
  * @example
  * keep([ 'a', null, '', 2]) // => [ 'a', 2 ]
+ * keep({ a: '', b: null, c: 2 }) // => { c: 2 }
  */
 
 const keep = (a) =>
--- src/keep.test.js
+++ src/keep.test.js
@@ -4,7 +4,9 @@ import keep from './keep'
 test('keep', (t) => {
   const withF = [ 'a', null, '', 2 ]
   const withoutF = [ 1, 2, 'a', 'b' ]
-  t.deepEqual(keep(withF), [ 'a', 2 ], 'strips falsey values')
+  const o = { a: '', b: null, c: 2 }
+  t.deepEqual(keep(withF), [ 'a', 2 ], 'strips falsey values from array')
+  t.deepEqual(keep(o), { c: 2 }, 'works for objects')
   t.deepEqual(keep(withoutF), withoutF, 'leaves all truthy values')
   t.end()
 })
