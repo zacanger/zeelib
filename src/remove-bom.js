316727edd1a2be7d9a3d48542b4c54d5b7db2a2b
zacanger <zac@zacanger.com>
2017-08-12 18:28:06 -0600

    Add removeBOM

---
+++ src/remove-bom.js
@@ -0,0 +1,19 @@
+// @flow
+
+/**
+ * The same as what's in Node's `module` (see `lib/internal/module`).
+ * Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+ * because the buffer-to-string conversion in `fs.readFileSync()`
+ * translates it to FEFF, the UTF-16 BOM.
+ * @example
+ * removeBOM(someContent)
+ */
+
+const removeBOM = (content: string): string => {
+  if (content.charCodeAt(0) === 0xFEFF) {
+    content = content.slice(1)
+  }
+  return content
+}
+
+export default removeBOM
