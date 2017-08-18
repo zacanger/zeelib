bafff99723dea2ed1aee471963b8a49827cd07ac
zacanger <zac@zacanger.com>
2017-08-18 15:04:25 -0600

    Simplify lint setup

--- src/each.test.js
+++ src/each.test.js
@@ -1,10 +1,10 @@
 import test from 'tape'
 import each from './each'
+import id from './id'
 
 test('each', (t) => {
-  const a = ['x', 'y', 'z']
-  const o = { a : 'a', b : 'b', c : 'c' }
-  const id = (b) => b
+  const a = [ 'x', 'y', 'z' ]
+  const o = { a: 'a', b: 'b', c: 'c' }
   t.is(each(), undefined, 'returns undefined with no args')
   t.is(each(a, id), 'x', 'returns first in passed arr with passed id fn')
   t.is(each(o, id), 'a', 'returns same with obj')
