3fd50994e35efc83883e4e3999d251283d0596b5
zacanger <zac@zacanger.com>
2017-08-25 10:55:08 -0600

    Add promisify

---
+++ src/promisify.js
@@ -0,0 +1,17 @@
+// @flow
+
+type AnyF = (any) => any
+
+/**
+ * Promisify a thing
+ * @example
+ * promisify(fs.readdir)(args)
+ */
+
+const promisify = (f: AnyF) =>
+  (...args: any[]) =>
+    new Promise((resolve: AnyF, reject: AnyF) =>
+      (f)(...args, (err: any, result: any) =>
+        err ? reject(err) : resolve(result)))
+
+export default promisify
