fab7443561597752fb903fe77168920243fbaab9
zacanger <zac@zacanger.com>
2017-09-08 17:14:12 -0600

    Add findIndices

---
+++ src/find-indices.js
@@ -0,0 +1,17 @@
+// @flow
+
+/**
+ * Find all indices of an item in an array
+ * @example
+ * findIndices(1, [ 1, 2, 1 ]) // => [ 0, 2 ]
+ */
+
+const findIndices = (el: any, arr: any[]): number[] =>
+  arr.reduce((a, e, i) => {
+    if (e === el) {
+      a.push(i)
+    }
+    return a
+  })
+
+export default findIndices
