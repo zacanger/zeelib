e464297b767f556ea6da83fc200608ee72a48502
zacanger <zac@zacanger.com>
2017-09-27 13:48:43 -0600

    Adjust concat.

--- src/concat.js
+++ src/concat.js
@@ -1,5 +1,7 @@
 // @flow
 
+import isArray from './is-array'
+
 /**
  * Like Haskell's `concat`,
  * a one-level flatten of an array of arrays
@@ -8,7 +10,14 @@
  * concat(nested) // => [ 1, 2, 3, [ 4, 5, 6 ] ]
  */
 
-const concat = (a: Array<any[]>): any[] =>
-  a.reduce((a: any[], b: any[]) => a.concat(b))
+const concat = (a: Array<any[]>): any[] => {
+  let res = []
+  a.forEach((e) => {
+    if (isArray(e)) {
+      res = res.concat(e)
+    }
+  })
+  return res
+}
 
 export default concat
