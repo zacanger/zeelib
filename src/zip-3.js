e0940f2ef5a2293971d9c824aba94d2bfe1603fb
zacanger <zac@zacanger.com>
2017-11-18 08:51:09 -0700

    More chores.

--- src/zip-3.js
+++ src/zip-3.js
@@ -1,26 +1,24 @@
+// @flow
+
 /**
  * Makes a list of tuples
  *
  * Each list is made of the elements of each list param's
  * elements at the same position
- * @param {array} xs
- * @param {array} ys
- * @param {array} zs
- * @returns {array}
  * @example
  * zip3([ 1, 2 ], [ 3, 4 ], [ 5, 6 ]) // => [ [ 1, 3, 5 ], [ 2, 4, 6 ] ]
  */
 
-const zip3 = (xs, ys, zs) => {
+const zip3 = (xs: any[], ys: any[], zs: any[]): Array<any[]> => {
   const res = []
   const yslen = ys.length
   const zslen = zs.length
-  for (let i in xs) {
+  xs.forEach((_, i) => {
     if (i >= yslen || i >= zslen) {
       return res
     }
     res.push([ xs[i], ys[i], zs[i] ])
-  }
+  })
   return res
 }
 
