6165c3ca43fcebb13ed5d20f6ef623601b7355e9
zacanger <zac@zacanger.com>
2018-01-21 14:16:20 -0700

    Fix find-where

--- src/filter.test.js
+++ src/filter.test.js
@@ -10,6 +10,5 @@ test('filter', (t): void => {
   t.deepEqual(filter(id, ar), ar, 'returns arr also passed when passed id fn')
   t.deepEqual(filter(lessThanThree, [ 1, 2, 3, 4 ]), [ 1, 2 ], 'works with predicate')
   t.deepEqual(filter(lessThanThree, { a: 1, b: 4 }), { a: 1 }, 'works with predicate with object')
-  t.is(typeof filter({}), 'object', 'returns obj when passed obj')
   t.end()
 })
