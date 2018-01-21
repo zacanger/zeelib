3908e05ecc235a56b25664bbdea37139591ad7bb
zacanger <zac@zacanger.com>
2018-01-21 14:59:27 -0700

    eff flow sometimes

--- src/clone.js
+++ src/clone.js
@@ -23,10 +23,10 @@ const clone = <T> (obj: T): T => {
     return new obj.constructor(obj)
   // Array and Object
   } else {
-    let copy = isArray(obj) ? [] : Object.create(Object.getPrototypeOf(obj))
+    let copy: T = isArray(obj) ? [] : Object.create(Object.getPrototypeOf(obj))
     for (let key in obj) {
       if (obj.hasOwnProperty(key)) {
-        copy[key] = this.clone(obj[key])
+        copy[key] = clone(obj[key])
       }
     }
     return copy
