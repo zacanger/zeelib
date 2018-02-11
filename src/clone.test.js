8ae1b5dd1b341c4b78af0928098c017c7b70c39d
zacanger <zac@zacanger.com>
2018-02-10 21:39:02 -0700

    Better test

--- src/clone.test.js
+++ src/clone.test.js
@@ -4,6 +4,13 @@ import test from 'tape'
 import clone from './clone'
 
 test('clone', (t): void => {
-  t.equal(clone(1), 1, 'works')
+  t.equal(clone(1), 1, 'works for number')
+  t.equal(clone('a'), 'a', 'works for string')
+  t.equal(clone(null), null, 'works for null')
+  t.equal(clone(true), true, 'works for bool')
+  t.deepEqual(clone(/foo/), /foo/, 'works for regexp')
+  t.deepEqual(clone(new Date('2018-01-01')), new Date('2018-01-01'), 'works for date')
+  t.deepEqual(clone([ 1 ]), [ 1 ], 'works for array')
+  t.deepEqual(clone({ a: 1 }), { a: 1 }, 'works for object')
   t.end()
 })
