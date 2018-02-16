eee0ef4d98c4d04d49a49aefcb5493db890226e9
Tom Pridham <pridham.tom@gmail.com>
2018-02-15 19:54:08 -0700

    revert some unquoted numbers

--- src/count-items-in-array.test.js
+++ src/count-items-in-array.test.js
@@ -4,6 +4,6 @@ import test from 'tape'
 import countItemsInArray from './count-items-in-array'
 
 test('countItemsInArray', (t): void => {
-  t.deepEqual(countItemsInArray([ 1, 1, 2, 3, 4 ]), { 1: 2, 2: 1, 3: 1, 4: 1 }, 'works')
+  t.deepEqual(countItemsInArray([ 1, 1, 2, 3, 4 ]), { '1': 2, '2': 1, '3': 1, '4': 1 }, 'works')
   t.end()
 })
