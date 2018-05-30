76d4f24cbefc1d25e33dc867445e0a0ff2e99d40
Tom Pridham <pridham.tom@gmail.com>
2018-05-30 11:56:00 -0600

    fix words to return an empty array when passed an empty string and fixed the test script in package.json (#29)

--- src/words.test.js
+++ src/words.test.js
@@ -5,5 +5,6 @@ import words from './words'
 
 test('words', (t): void => {
   t.deepEqual(words('foo bar'), [ 'foo', 'bar' ], 'makes arr split on space')
+  t.deepEqual(words(''), [], 'returns an empty array for an empty string')
   t.end()
 })
